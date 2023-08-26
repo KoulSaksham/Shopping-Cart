b70c5168fd1bf4729e65cc328d2ee5b3accd01b8
KoulSaksham <skoul098@gmail.com>
2023-08-26 11:48:20 +0530

    Update CheckOut.java

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -5,4 +5,5 @@ interface CheckOut
     double addTotal(HashMap<String,String> list);
     double discountCheck(HashMap<String,String> list);
     double finalBill();
+    //this is the change added with vscode
 }
