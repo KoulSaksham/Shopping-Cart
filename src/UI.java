9ecdb38de0d2e82ddd87895185f0e093c5effa38
KoulSaksham <skoul098@gmail.com>
2023-09-02 00:24:09 +0530

    Major changes in the backbone of the project
    In this latest round of updates, a significant enhancement has been made to the user's shopping list – the addition of a robust deletion functionality. Notably, the storage mechanism for the User List within the HashMap has been refined to ensure seamless execution during deletion operations, eliminating potential errors.
    
    Furthermore, to enhance the overall program's reliability, additional try-catch blocks have been strategically placed in the item addition function, guaranteeing smooth and error-free execution.
    
    To optimize the user experience, threads have been introduced for the parallel display of both the User List and Our List, delivering a more efficient and responsive application.

--- src/UI.java
+++ src/UI.java
@@ -6,24 +6,31 @@ class UI implements UserCart {
 
     UI() {
         OurList = new ArrayList<>();
-        OurList.add("1. Watches");
-        OurList.add("2. Goggles");
-        OurList.add("3. T-shirts");
-        OurList.add("4. Formal Shirt"); //formal shirt 3
-        OurList.add("5. Jeans");
-        OurList.add("6. Pants");
-        OurList.add("7. Shorts");
-        OurList.add("8. Shoes");
-        OurList.add("9. Socks");
+        OurList.add("Watches");
+        OurList.add("Goggles");
+        OurList.add("T-shirt");
+        OurList.add("Iphone");
+        OurList.add("Jeans");
+        OurList.add("Pants");
+        OurList.add("Shorts");
+        OurList.add("Shoes");
+        OurList.add("Socks");
     }
 
     @Override
     public void displayOurList() {
-        for (String item : OurList)
-            System.out.println("\t\t\t\t\t\t"+item);
+        int blackStartCode = 0x2605; // Unicode code point for black star point
+        for (String item : OurList) {
+            String bullet = new String(Character.toChars(blackStartCode));
+            System.out.println("\t\t\t\t\t\t" + bullet + " " + item);
+            try {
+                Thread.sleep(500); // 1000 milliseconds = 1 second
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
-            //shorts 2
-            //pants 2
+
     @Override
     public void addItemToUserList(int quantity, String item) {
         String key = item + "_" + quantity;
@@ -31,11 +38,44 @@ class UI implements UserCart {
     }
 
     @Override
-    public void removeItemFromUserList(String item) {
-        if (!customerOrder.isEmpty())
-            customerOrder.remove(item);
-        else
-            System.out.println("\t\t\t\t\t\tUser list is empty.");
+    public void removeItemFromUserList(HashMap<String, String> customerOrder, int inputQuantity, String itemName) {
+        if (!customerOrder.isEmpty()) {
+            for (Map.Entry<String, String> mapElement : customerOrder.entrySet()) {
+                String key = mapElement.getKey();
+                String value = mapElement.getValue();
+                String[] parts = key.split("_");
+                int quantity = Integer.parseInt(parts[1]);
+                if (value.equalsIgnoreCase(itemName)) {
+                    if (inputQuantity < quantity) {
+                        int newQuantity = quantity - inputQuantity;
+                        customerOrder.remove(key);
+                        customerOrder.put(itemName + "_" + newQuantity, itemName);
+                        System.out.println("\t\t\t\t\t\tDeletion successful.");
+                        System.out.println("\t\t\t\t\t\t══════════════════════════════════════════════════");
+                        System.out.println("\t\t\t\t\t\tUpdated cart elements are:-");
+                        System.out.println("\t\t\t\t\t\t══════════════════════════════════════════════════");
+                        displayUserList();
+                        return; // Exit after successful deletion
+                    } else if (inputQuantity == quantity) {
+                        customerOrder.remove(key);
+                        System.out.println("\t\t\t\t\t\t══════════════════════════════════════════════════");
+                        System.out.println("\t\t\t\t\t\tDeletion successful.");
+                        System.out.println("\t\t\t\t\t\t══════════════════════════════════════════════════");
+                        System.out.println("\t\t\t\t\t\tUpdated cart elements are:-");
+                        displayUserList();
+                        System.out.println("\t\t\t\t\t\t══════════════════════════════════════════════════");
+                        return; // Exit after successful deletion
+                    } else {
+                        System.out.println("\t\t\t\t\t\tQuantity entered is greater than stored quantity.");
+                        System.out.println("\t\t\t\t\t\tCannot delete the item!!");
+                        return; // Exit , no need to check other entries
+                    }
+                }
+            }
+            System.out.println("\t\t\t\t\t\tSorry ! Item not found in the cart.");
+            System.out.println("\t\t\t\t\t\t══════════════════════════════════════════════════");
+        } else
+            System.out.println("\t\t\t\t\t\tUser list is empty. Cannot delete");
     }
 
     @Override
@@ -46,83 +86,66 @@ class UI implements UserCart {
                 String value = mapElement.getValue();
                 String[] parts = key.split("_");
                 int quantity = Integer.parseInt(parts[1]);
-                System.out.println("\t\t\t\t\t\t"+quantity + " : " + value);
+                System.out.println("\t\t\t\t\t\t" + quantity + " : " + value);
+                try {
+                    Thread.sleep(500); // 1000 milliseconds = 1 second
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-           // System.out.println("storage in hashmap-> "+customerOrder);
         } else
             System.out.println("\t\t\t\t\t\tUser List is Empty. Cannot display the data.");
+    }
+
+    @Override
+    public boolean checkItemName(String item) {
+        boolean isValid = false;
+        for (String element : OurList) {
+            if (item.equalsIgnoreCase(element)) {
+                return true;
+            }
+        }
+        return isValid;
     }
 
     public void addItemsRecursively(char reply) {
         if (reply == 'y' || reply == 'Y') {
             Scanner sc = new Scanner(System.in);
-            HashMap<String, String> tempOrder = new HashMap<>();  //create a temporary HM to avoid the key-value pairs
-            // accumulate with each recursive call, leading to overlapping entries
+            HashMap<String, String> tempOrder = new HashMap<>(); //can be moved on top as private
+            /* create a temporary HM to avoid the
+               key-value pairs to
+               accumulate with each recursive call, thus leading to overlapping entries */
+            System.out.println("\t\t\t\t\t\t══════════════════════════════════════════════════");
             displayOurList();
+            System.out.println("\t\t\t\t\t\t══════════════════════════════════════════════════");
             System.out.println("\t\t\t\t\t\tEnter item name followed by the quantity :-");
             String answer = sc.nextLine();
             String[] items = answer.split(" ");
-            int quantity = Integer.parseInt(items[1]);
-            String value = items[0];
-            tempOrder.putAll(customerOrder); // Copy existing entries to the new HashMap
-            tempOrder.put(value + "_" + quantity, value); // Add the new entry
-            customerOrder = tempOrder; // Update the customerOrder reference
-            System.out.println("\t\t\t\t\t\tYour cart elements are as follows:-");
-            displayUserList();
+            try {
+                if (items.length != 2) {
+                    throw new ArrayIndexOutOfBoundsException();
+                }
+                int quantity = Integer.parseInt(items[1]);
+                String value = items[0];
+                tempOrder.putAll(customerOrder); // Copy existing entries to the new LinkedHashMap
+                tempOrder.put(value + "_" + quantity, value); // Add the new entry
+                customerOrder = tempOrder; // Update the customerOrder reference
+            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                System.out.println("\t\t\t\t\t\t❌ Invalid input .");
+                return;
+            }
+            System.out.println("\t\t\t\t\t\t══════════════════════════════════════════════════");
+            System.out.println("\t\t\t\t\t\tItem added successfully !!");
+            System.out.println("\t\t\t\t\t\t══════════════════════════════════════════════════");
             System.out.println("\t\t\t\t\t\tDo you want to add more items in your cart? ( Y or N )");
             reply = sc.next().charAt(0);
             addItemsRecursively(reply);
         } else if (reply == 'N' || reply == 'n') {
             System.out.println("\t\t\t\t\t\tOk. Printing your cart now.");
             displayUserList();
+            System.out.println("\t\t\t\t\t\t══════════════════════════════════════════════════");
         } else {
             System.out.println("\t\t\t\t\t\tBad Choice !!");
         }
     }
 }
-    /*public void addAgain(char reply2) {
-        if (reply2 == 'y' || reply2 == 'Y') {
-            Scanner sc = new Scanner(System.in);
-            //sc.nextLine();
-            displayOurList();
-            System.out.println("Enter item name followed by the quantity :-");
-            String answer2 = sc.nextLine();
-            String[] values2 = answer2.split(" ");
-            int quantity2 = Integer.parseInt(values2[1]);
-            String value2 = values2[0];
-            this.addItemToUserList(quantity2, value2);
-            System.out.println("Your cart elements are as follows:-");
-            this.displayUserList();
-            System.out.println("Do you want to add more items in your cart? ( Y or N )");
-            char reply3 = sc.next().charAt(0);
-
-            // Consume the newline character left in the buffer
-            sc.nextLine();
-
-            if (reply3 == 'Y'  || reply3 == 'y' ) {
-                displayOurList();
-                System.out.println("Enter item name followed by the quantity :-"); // shoes 2
-                String answer3 = sc.nextLine(); //sc.next
-                String[] values3 = answer3.split(" ");
-                int quantity3 = Integer.parseInt(values3[1]);
-                String items = values3[0];
-                this.addItemToUserList(quantity2, items);
-                System.out.println("Limit Reached !!");
-                System.out.println("Items in your cart are:-");
-                this.displayUserList();
-
-            } else if (reply3 == 'N' || reply3 == 'n') {
-                System.out.println("Ok. Printing your cart now.");
-                this.displayUserList();
-            } else {
-                System.out.println("Bad Choice !!");
-            }
-            sc.close();
-        } else {
-            System.out.println("Invalid Choice BYE !!");
-        }
-    } */
-
-
-
-
