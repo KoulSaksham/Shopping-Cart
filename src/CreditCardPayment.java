fda4f15a175876aefe94dba0ed9d34033f56aa5c
KoulSaksham <skoul098@gmail.com>
2023-09-02 00:33:49 +0530

    Some beautification changes to this class
    Few lines of code have been added in this class to make the billing section of the project look more elegant than before.

--- src/CreditCardPayment.java
+++ src/CreditCardPayment.java
@@ -1,10 +1,14 @@
 
 class CreditCardPayment implements PaymentMethod {
     private final String cardNumber;
+
     public CreditCardPayment(String cardNumber) {
         this.cardNumber = cardNumber;
     }
+
     public void pay(double amount) {
-        System.out.println("\t\t\t\t\t\tPaid $" + amount + " with credit card " + cardNumber);
+        System.out.printf("\t\t\t\t\t\tPaid $" + "%.3f", amount);
+        System.out.print(" with credit card number " + cardNumber + ".");
+        System.out.println();
     }
 }
